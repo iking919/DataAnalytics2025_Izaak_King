96244da27b7bc6f2419024f9474382817dff052a
iking919 <153115930+iking919@users.noreply.github.com>
2025-09-30 20:34:45 -0400

    Problems 1-2 done

--- assign2/assignment2.R
+++ assign2/assignment2.R
@@ -1,6 +1,7 @@
 library(readr)
 library(EnvStats)
 library(nortest)
+library(ggplot2)
 
 # set working directory (relative path)
 setwd("C:/Users/kingi/Documents/github/DataAnalytics2025_Izaak_king/assign2")
@@ -39,12 +40,79 @@ qqplot(HLT.GlobalWest, HLT.EasternEurope,
 lin.mod0 <- lm(HLT.new~population,epi.data)
 summary(lin.mod0)
 
-lin.mod0 <- lm(HLT.new~gdp,epi.data)
-summary(lin.mod0)
+lin.mod1 <- lm(HLT.new~gdp,epi.data)
+summary(lin.mod1)
 
 
 # remove the outliers in our population data using boxplot Formula 
 # one line method found in https://www.geeksforgeeks.org/r-language/outlier-analysis-in-r/ (step 3)
 
-filtered.data <- epi.data[!epi.data$population %in% boxplot.stats(epi.data$population)$out, ]
+filteredPop.data <- epi.data[!epi.data$population %in% boxplot.stats(epi.data$population)$out, ]
+lin.mod2 <- lm(HLT.new~population, filteredPop.data)
+summary(lin.mod2)
+
+filteredGDP.data <- epi.data[!epi.data$gdp %in% boxplot.stats(epi.data$gdp)$out, ]
+lin.mod3 <- lm(HLT.new~gdp, filteredGDP.data)
+summary(lin.mod3)
+
+
+# now filter data using log
+
+epi.data$log_Population <- log10(epi.data$population)
+lin.mod4 <- lm(HLT.new~log_Population, epi.data)
+summary(lin.mod4)
+
+epi.data$log_GDP <- log10(epi.data$gdp)
+lin.mod5 <- lm(HLT.new~log_GDP, epi.data)
+summary(lin.mod5)
+
+
+#Plot our two best models, lin.mod3 and lin.mod4.
+
+ggplot(epi.data, aes(x = log_Population, y = HLT.new)) +
+  geom_point() +
+  stat_smooth(method = "lm")
+
+ggplot(lin.mod4, aes(x = .fitted, y = .resid)) +
+  geom_point() +
+  geom_hline(yintercept = 0) +
+  labs(title='Residual vs. Fitted Values Plot', x='Fitted Values', y='Residuals')
+
+ggplot(filteredGDP.data, aes(x = gdp, y = HLT.new)) +
+  geom_point() +
+  stat_smooth(method = "lm")
+
+ggplot(lin.mod3, aes(x = .fitted, y = .resid)) +
+  geom_point() +
+  geom_hline(yintercept = 0) +
+  labs(title='Residual vs. Fitted Values Plot', x='Fitted Values', y='Residuals')
+
+# now make models for Eastern Europe
+
+# first using log population 
+
+epi.data.EasternEurope$log_Population <- log10(epi.data.EasternEurope$population)
+lin.mod6 <- lm(HLT.new~log_Population, epi.data.EasternEurope)
+summary(lin.mod6)
+ggplot(epi.data.EasternEurope, aes(x = log_Population, y = HLT.new)) +
+  geom_point() +
+  stat_smooth(method = "lm")
+ggplot(lin.mod6, aes(x = .fitted, y = .resid)) +
+  geom_point() +
+  geom_hline(yintercept = 0) +
+  labs(title='Residual vs. Fitted Values Plot', x='Fitted Values', y='Residuals')
+
+# now using GDP with no outliers
+
+filteredGDP.data.EasternEurope <- epi.data.EasternEurope[!epi.data.EasternEurope$gdp %in% boxplot.stats(epi.data.EasternEurope$gdp)$out, ]
+lin.mod7 <- lm(HLT.new~gdp, filteredGDP.data.EasternEurope)
+summary(lin.mod7)
+ggplot(filteredGDP.data.EasternEurope, aes(x = gdp, y = HLT.new)) +
+  geom_point() +
+  stat_smooth(method = "lm")
+ggplot(lin.mod7, aes(x = .fitted, y = .resid)) +
+  geom_point() +
+  geom_hline(yintercept = 0) +
+  labs(title='Residual vs. Fitted Values Plot', x='Fitted Values', y='Residuals')
+
 
