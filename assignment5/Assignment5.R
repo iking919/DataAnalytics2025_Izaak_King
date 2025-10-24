9eef617d3c4bb84913f7bb7b27ce38ac8131ba09
iking919 <153115930+iking919@users.noreply.github.com>
2025-10-23 21:19:49 -0400

    Added question 2

--- assignment5/Assignment5.R
+++ assignment5/Assignment5.R
@@ -248,7 +248,101 @@ print(paste("Amount Correct:", correct2))
 print(paste("RF Model 2 Accuracy:", round(accuracy2, 3)))
 
 
-# For Question 2
 
-Queens.data <- NYC.data[NYC.data$BOROUGH == 'QUEENS', ]
+### QUESTION 2 ###
+
+### DATASET CLEANING ###
+Queens.uncleaned.data <- NYC.data[NYC.data$BOROUGH == 'QUEENS', ]
+Queens.data <- Queens.uncleaned.data
+
+# Convert square feet to numeric values
+Queens.data$LAND.SQUARE.FEET <- as.numeric(gsub(",", "", Queens.data$LAND.SQUARE.FEET))
+Queens.data$GROSS.SQUARE.FEET <- as.numeric(gsub(",", "", Queens.data$GROSS.SQUARE.FEET))
+
+# Convert sale date to numeric
+Queens.data$SALE.DATE <- mdy(Queens.data$SALE.DATE)
+Queens.data$SALE.DATE.NUM <- as.numeric(Queens.data$SALE.DATE)
+
+# Remove general outliers
+Queens.data <- Queens.data %>%
+  filter(!is.na(YEAR.BUILT) & !is.na(LAND.SQUARE.FEET) & 
+           !is.na(SALE.DATE) & !is.na(SALE.PRICE) & !is.na(NEIGHBORHOOD)&
+           !is.na(GROSS.SQUARE.FEET)& YEAR.BUILT > 1850 & LAND.SQUARE.FEET > 0 & 
+           GROSS.SQUARE.FEET > 0 & 1000000 >= LAND.SQUARE.FEET & 
+           1000000 >= GROSS.SQUARE.FEET & 900000000 >= SALE.PRICE & SALE.PRICE > 10000)
+
+# Log cleaning for sale price, square feet attributes
+Queens.data$LOG.SALE.PRICE <- log10(Queens.data$SALE.PRICE)
+Queens.data$LOG.LAND.SQUARE.FEET <- log10(Queens.data$LAND.SQUARE.FEET)
+Queens.data$LOG.GROSS.SQUARE.FEET <- log10(Queens.data$GROSS.SQUARE.FEET)
+
+### APPLYING THE REGRESSION MODELS ###
+
+#Applying lin.mod1
+predictions <- predict(lin.mod1, newdata = Queens.data)
+residuals <- Queens.data$LOG.SALE.PRICE - predictions
+
+
+ggplot(Queens.data, aes(x = predictions, y = LOG.SALE.PRICE)) +
+  geom_point(alpha = 0.6) +
+  stat_smooth(method = "lm", color = "blue") +
+  labs(title = "Predicted vs Actual Sale Price (Queens)",
+       x = "Predicted LOG.SALE.PRICE",
+       y = "Actual LOG.SALE.PRICE")
+
+ggplot(data.frame(Predicted = predictions, Residuals = residuals), aes(x = Predicted, y = Residuals)) +
+  geom_point(alpha = 0.6) +
+  geom_hline(yintercept = 0, color = "red") +
+  labs(title = "Residuals vs Predicted Values (Queens)",
+       x = "Predicted LOG.SALE.PRICE",
+       y = "Residuals")
+
+
+#Applying lin.mod6
+predictions <- predict(lin.mod6, newdata = Queens.data)
+residuals <- Queens.data$LOG.SALE.PRICE - predictions
+
+
+ggplot(Queens.data, aes(x = predictions, y = LOG.SALE.PRICE)) +
+  geom_point(alpha = 0.6) +
+  stat_smooth(method = "lm", color = "blue") +
+  labs(title = "Predicted vs Actual Sale Price (Queens)",
+       x = "Predicted LOG.SALE.PRICE",
+       y = "Actual LOG.SALE.PRICE")
+
+ggplot(data.frame(Predicted = predictions, Residuals = residuals), aes(x = Predicted, y = Residuals)) +
+  geom_point(alpha = 0.6) +
+  geom_hline(yintercept = 0, color = "red") +
+  labs(title = "Residuals vs Predicted Values (Queens)",
+       x = "Predicted LOG.SALE.PRICE",
+       y = "Residuals")
+
+
+### APPLYING THE SUPERVISED LEARNING MODELS ###
+
+# First applying best performing knn model 2
+model2InputQueens <- scale(Queens.data[, c("LOG.SALE.PRICE","LOG.GROSS.SQUARE.FEET", "LOG.LAND.SQUARE.FEET")])
+labelManhattan <- Manhattan.data$NEIGHBORHOOD
+knnModel2Queens <- knn(train = model2Input, test = model2InputQueens, cl = labelManhattan, k = 38)
+
+# Evaluate
+confusionMatrixQueens <- table(Predicted = knnModel2Queens, Actual = Queens.data$NEIGHBORHOOD)
+correct <- sum(diag(confusionMatrixQueens))
+total <- nrow(Queens.data)
+accuracy <- correct / total
+print(paste("Amount Correct:", correct))
+print(paste("KNN Model 2 Accuracy on Queens:", round(accuracy, 3)))
+
+
+# Now applying Random Forest
+Queens.data$NEIGHBORHOOD <- as.factor(Queens.data$NEIGHBORHOOD)
+predRFQueens <- predict(rf.model2, newdata = Queens.data)
+
+# Evaluate
+confusionMatrixRFQueens <- table(Predicted = predRFQueens, Actual = Queens.data$NEIGHBORHOOD)
+correctRF <- sum(diag(confusionMatrixRFQueens))
+total <- nrow(Queens.data)
+accuracyRF <- correctRF / total
+print(paste("Amount Correct:", correctRF))
+print(paste("RF Model 2 Accuracy on Queens:", round(accuracyRF, 3)))
 
