9f4e0cd9c5ea17a87afd3df6ecc0f4eae4a25591
iking919 <153115930+iking919@users.noreply.github.com>
2025-10-24 11:36:28 -0400

    fixed name

--- lab4/lab4.R
+++ lab4/lab4.R
@@ -8,6 +8,7 @@ library(ggfortify)
 library(GGally)
 library(e1071)
 library(class)
+library(caret)
 library(psych)
 library(readr)
 
@@ -34,9 +35,81 @@ head(wine)
 wine$Type <- as.factor(wine$Type)
 
 
-## visualize variables
-pairs.panels(wine[,-1],gap = 0,bg = c("red", "yellow", "blue")[wine$Type],pch=21)
-
-ggpairs(wine, ggplot2::aes(colour = Type))
-
 ###
+
+# creating another dataframe from wine dataset that contains the identifier column 1,and another with all other attributes
+X <- wine[,-1]
+Y <- wine[,1]
+
+
+
+####### PCA #######
+
+Xmat <- as.matrix(X)
+
+Xc <- scale(Xmat, center = T, scale = T)
+
+principal_components <- princomp(Xc)
+
+summary(principal_components)
+
+principal_components$loadings
+
+# plotting the principal_components using the a line in plot() functions 
+plot(principal_components, type = "l")
+
+## using autoplot() function to plot the components
+autoplot(principal_components, data = wine, colour = 'Type',
+         loadings = TRUE, loadings.colour = 'blue',
+         loadings.label = TRUE, loadings.label.size = 3, scale = 0)
+
+
+
+
+# knn model based on all attributes,
+model0Input <- scale(wine[, c("Alcohol","Malic acid","Ash","Alcalinity of ash","Magnesium","Total phenols","Flavanoids","Nonflavanoid Phenols","Proanthocyanins","Color Intensity","Hue","Od280/od315 of diluted wines","Proline")])
+label <- wine$Type
+
+knnModel0 <- knn(train = model0Input, test = model0Input, cl = label, k = 3)
+
+# Print performance statistics
+confusionMatrix0 <- table(Predicted = knnModel0, Actual = label)
+correct <- sum(diag(confusionMatrix0)) 
+total <- length(wine$Type)
+accuracy <- correct / total
+print(confusionMatrix0)
+print(paste("Amount Correct:", correct))
+print(paste("Accuracy:", round(accuracy, 3)))
+
+#Getting F1
+cm <- confusionMatrix(knnModel0, as.factor(label), mode = "everything")
+precision <- cm$byClass[, "Precision"]
+recall <- cm$byClass[, "Recall"]
+f1 <- 2 * (precision * recall) / (precision + recall)
+print(f1)
+
+
+
+
+# knn model using the first 2 PCs (Score)
+
+pc_scores <- principal_components$scores[, 1:2]
+
+knnModel1 <- knn(train = pc_scores, test = pc_scores, cl = label, k = 3)
+
+# Print performance statistics
+confusionMatrix1 <- table(Predicted = knnModel1, Actual = label)
+correct <- sum(diag(confusionMatrix1)) 
+total <- length(wine$Type)
+accuracy <- correct / total
+print(confusionMatrix1)
+print(paste("Amount Correct:", correct))
+print(paste("Accuracy:", round(accuracy, 3)))
+
+#Getting F1
+cm <- confusionMatrix(knnModel1, as.factor(label), mode = "everything")
+precision <- cm$byClass[, "Precision"]
+recall <- cm$byClass[, "Recall"]
+f1 <- 2 * (precision * recall) / (precision + recall)
+print(f1)
+
